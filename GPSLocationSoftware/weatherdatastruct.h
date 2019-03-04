50c26905e398687992b55764091f5910f71df7f1
jagajda <32704908+jagajda@users.noreply.github.com>
2019-03-04 21:39:21 +0100

    Update weatherdatastruct.h
    add comments

--- GPSLocationSoftware/weatherdatastruct.h
+++ GPSLocationSoftware/weatherdatastruct.h
@@ -4,40 +4,40 @@
 #include <QString>
 typedef struct WeatherDataStruct{
 
-    //cord
-    int16_t lon;
-    int16_t lat;
+    //cordinates
+    int16_t lon;    //logtitude
+    int16_t lat;    //latitude
 
     //weather
-    uint8_t id;
-    QString main;
-    QString description;
-    QString icon;
+    uint8_t id; //Weather condition id
+    QString main;   //group of weather parameters (Rain, Snow, Extreme etc.)
+    QString description;    //Weather condition within the group
+    QString icon;   //icon- if needed
 
-    QString  base;
+    //QString  base;
 
     //main
     int8_t temp;
     uint8_t pressure;
     uint8_t humidity;
-    int8_t temp_min;
-    int8_t temp_max;
+    int8_t temp_min;    //minimum temperature at the moment
+    int8_t temp_max;    // maximum temperature at the moment
 
-    int8_t visibility;
+    int8_t visibility;  //visibility, meter
 
     //wind
     uint8_t speed;
     uint8_t deg;
 
     //clouds
-    uint8_t all;
+    uint8_t all;    //cloudiness
 
     //dt
-    uint16_t dt;
+    uint16_t dt;    //time of data calculation, unix, UTC 
 
     //sys
-    QString country;
-    QString name;
+    QString country;    //country code (GB, PL etc.)
+    QString name;       //city name 
 }Weather;
 
 #endif // WEATHERDATASTRUCT_H
